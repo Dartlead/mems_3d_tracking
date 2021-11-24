99f1ec4f72aeb748848b2c98b74d4b00b9a3edd0
dartlead <nickcessna@gmail.com>
2021-11-24 00:55:52 -0800

    All LEDs

--- src/drivers/onboard_led.cpp
+++ src/drivers/onboard_led.cpp
@@ -3,9 +3,9 @@
 #include "dartlead_assert.h"
 #include <cstddef>
 
-static GPIO::pin * PB0  = NULL;
-static GPIO::pin * PB7  = NULL;
-static GPIO::pin * PB14 = NULL;
+//static GPIO::pin * PB0  = NULL;
+//static GPIO::pin * PB7  = NULL;
+//static GPIO::pin * PB14 = NULL;
 
 /**
  * @note Green LED is connected to PB0.
@@ -13,106 +13,108 @@ static GPIO::pin * PB14 = NULL;
  * @note Red LED is connected to PB14.
  */
 onboard_LED::onboard_LED(LED::color const color) {
-	switch (color) {
-		case LED::color::green:
-			if (PB0 != NULL) {
-				PB0 = new GPIO::pin(GPIO::port::B, 0);
-				color_member = LED::color::green;
-			}
-			else {
-				//Do nothing
-			}
-			break;
-		case LED::color::blue:
-			if (PB7 != NULL) {
-				PB7 = new GPIO::pin(GPIO::port::B, 7);
-				color_member = LED::color::blue;
-			} else {
-				//Do nothing
-			}
-			break;
-		case LED::color::red:
-			if (PB14 != NULL) {
-				PB14 = new GPIO::pin(GPIO::port::B, 14);
-				color_member = LED::color::red;
-			} else {
-				//Do nothing
-			}
-			break;
-		default:
-			dartlead_WTF;
-			break;
-	}
+	//switch (color) {
+	//	case LED::color::green:
+	//		if (PB0 != NULL) {
+	//			PB0 = new GPIO::pin(GPIO::port::B, 0);
+	//			color_member = LED::color::green;
+	//		}
+	//		else {
+	//			//Do nothing
+	//		}
+	//		break;
+	//	case LED::color::blue:
+	//		if (PB7 != NULL) {
+	//			PB7 = new GPIO::pin(GPIO::port::B, 7);
+	//			color_member = LED::color::blue;
+	//		} else {
+	//			//Do nothing
+	//		}
+	//		break;
+	//	case LED::color::red:
+	//		if (PB14 != NULL) {
+	//			PB14 = new GPIO::pin(GPIO::port::B, 14);
+	//			color_member = LED::color::red;
+	//		} else {
+	//			//Do nothing
+	//		}
+	//		break;
+	//	default:
+	//		dartlead_WTF;
+	//		break;
+	//}
+	return;
 }
 
 onboard_LED::~onboard_LED() {
-	switch (color_member) {
-		case LED::color::green:
-			delete PB0;
-			break;
-		case LED::color::blue:
-			delete PB7;
-			break;
-		case LED::color::red:
-			delete PB14;
-			break;
-		default:
-			dartlead_WTF;
-			break;
-	}
+	//switch (color_member) {
+	//	case LED::color::green:
+	//		delete PB0;
+	//		break;
+	//	case LED::color::blue:
+	//		delete PB7;
+	//		break;
+	//	case LED::color::red:
+	//		delete PB14;
+	//		break;
+	//	default:
+	//		dartlead_WTF;
+	//		break;
+	//}
+	return;
 }
 
 LED::status onboard_LED::turn_on() {
-	switch (color_member) {
-		case LED::color::green:
-			PB0->write(1);
-			break;
-		case LED::color::blue:
-			PB7->write(1);
-			break;
-		case LED::color::red:
-			PB14->write(1);
-			break;
-		default:
-			dartlead_WTF;
-			break;
-	}
+	//switch (color_member) {
+	//	case LED::color::green:
+	//		PB0->write(1);
+	//		break;
+	//	case LED::color::blue:
+	//		PB7->write(1);
+	//		break;
+	//	case LED::color::red:
+	//		PB14->write(1);
+	//		break;
+	//	default:
+	//		dartlead_WTF;
+	//		break;
+	//}
 	return LED::status::ok;
 }
 
 LED::status onboard_LED::turn_off() {
-	switch (color_member) {
-		case LED::color::green:
-			PB0->write(0);
-			break;
-		case LED::color::blue:
-			PB7->write(0);
-			break;
-		case LED::color::red:
-			PB14->write(0);
-			break;
-		default:
-			dartlead_WTF;
-			break;
-	}
+	//switch (color_member) {
+	//	case LED::color::green:
+	//		PB0->write(0);
+	//		break;
+	//	case LED::color::blue:
+	//		PB7->write(0);
+	//		break;
+	//	case LED::color::red:
+	//		PB14->write(0);
+	//		break;
+	//	default:
+	//		dartlead_WTF;
+	//		break;
+	//}
 	return LED::status::ok;
 }
 
 LED::status onboard_LED::toggle() {
-	switch (color_member) {
-		case LED::color::green:
-			PB0->toggle();
-			break;
-		case LED::color::blue:
-			PB7->toggle();
-			break;
-		case LED::color::red:
-			PB14->toggle();
-			break;
-		default:
-			dartlead_WTF;
-			break;
-	}
+	//switch (color_member) {
+	//	case LED::color::green:
+	//		PB0->toggle();
+	//		break;
+	//	case LED::color::blue:
+	//		PB7->toggle();
+	//		break;
+	//	case LED::color::red:
+	//		PB14->toggle();
+	//		break;
+	//	default:
+	//		dartlead_WTF;
+	//		break;
+	//}
 	return LED::status::ok;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,45 @@
 #include "stm32f767zi.h"
 
-/*! FreeRTOS includes.
+/** FreeRTOS includes
  */
 #include "FreeRTOS.h"
 #include "task.h"
 
-/*! STM32F767ZI includes.
+/** STM32F767ZI includes
  */
-//#include "onboard_leds.h"
 #include "driver_GPIO.h"
 
 static void __attribute__((section(".text.fast_text"))) LED_toggle_task(void * parameter) {
 	TickType_t const delay_200ms    = pdMS_TO_TICKS(200UL);
 	TickType_t       last_wake_time = xTaskGetTickCount();
 
+	/** Init green LED */
 	GPIO::pin PB0(GPIO::port::B, (uint8_t)0);
 	PB0.set_mode(GPIO::mode::output);
 	PB0.set_output_type(GPIO::output_type::push_pull);
 	PB0.set_output_speed(GPIO::output_speed::low);
 	PB0.set_pull(GPIO::pull::none);
 
+	/** Init blue LED */
+	GPIO::pin PB7(GPIO::port::B, (uint8_t)7);
+	PB7.set_mode(GPIO::mode::output);
+	PB7.set_output_type(GPIO::output_type::push_pull);
+	PB7.set_output_speed(GPIO::output_speed::low);
+	PB7.set_pull(GPIO::pull::none);
+
+	/** Init red LED */
+	GPIO::pin PB14(GPIO::port::B, (uint8_t)14);
+	PB14.set_mode(GPIO::mode::output);
+	PB14.set_output_type(GPIO::output_type::push_pull);
+	PB14.set_output_speed(GPIO::output_speed::low);
+	PB14.set_pull(GPIO::pull::none);
+
 	while (1) {
 		vTaskDelayUntil(&last_wake_time, delay_200ms);
 
 		PB0.toggle();
+		PB7.toggle();
+		PB14.toggle();
 	}
 }
 
